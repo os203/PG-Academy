e4c40ab230e19591058515e34145f72c9b81d2f6
AhmadAymanALDAKhl <ad856658@gmail.com>
2026-04-29 20:31:39 +0300

    logo fixing

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -10,14 +10,14 @@ export default function Navbar() {
     <nav className="fixed top-0 w-full z-50 glass">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-20 items-center">
-          <div className="flex-shrink-0 flex items-center gap-2 hover-lift">
-            <div className="w-10 h-10 rounded-xl bg-gradient-to-br from-brand-accent to-brand-primary flex items-center justify-center text-white font-bold text-xl shadow-lg shadow-brand-accent/20">
-              P
+          <Link href="/" className="shrink-0 flex items-center gap-2 hover-lift rounded-xl ">
+            <div className="w-10 h-10 rounded-xl bg-linear-to-br from-brand-accent to-brand-primary flex items-center justify-center text-white font-bold text-xl shadow-lg shadow-brand-accent/20">
+              PG
             </div>
-            <Link href="/" className="font-bold text-2xl tracking-tight text-foreground">
+            <div className="font-bold text-2xl tracking-tight text-foreground pr-2">
               PG Academy
-            </Link>
-          </div>
+            </div>
+          </Link>
           <div className="hidden md:flex space-x-8 items-center">
             <Link href="/courses" className="text-foreground/80 hover:text-brand-accent transition-colors font-medium">Courses</Link>
             <Link href="/about" className="text-foreground/80 hover:text-brand-accent transition-colors font-medium">About</Link>
